437749330c40c0d224003c9d3738e0db9433a081
arrrtemida <artemlenin96@gmail.com>
2024-09-11 21:59:55 +0500

    сделал домашку №1

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Hello world!")
+
+if True:
+    pass
+
+'''
+Ctrl + Z – назад
+Ctrl + Y – вперёд
+Ctrl + X – вырезать
+Ctrl + C – копировать
+Ctrl + V - вставить
+Ctrl + D - дублировать
+Shift + Delete – удалить строку
+
+Ctrl + Alt + L - PEP8
+Shift + F6 – переименовать
+Shift + F10 – запустить
+'''
